cd15714d1292ceb0d1946483f85b32e5519b9fd2
Thifany-Nicastro <20716540+Thifany-Nicastro@users.noreply.github.com>
2023-12-12 09:22:13 -0300

    Postgres and model

---
+++ models/models.go
@@ -0,0 +1,10 @@
+package models
+
+import "gorm.io/gorm"
+
+type Task struct {
+    gorm.Model
+    Title string `json:"title" gorm:"text;not null;default:null`
+    Description   string `json:"description" gorm:"text;not null;default:null`
+    Status   string `json:"status" gorm:"text;not null;default:null`
+}
