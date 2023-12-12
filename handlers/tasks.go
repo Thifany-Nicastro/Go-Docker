dae1913df7b62d484339be207c2ad81e7cc57e86
Thifany-Nicastro <20716540+Thifany-Nicastro@users.noreply.github.com>
2023-12-12 11:01:17 -0300

    routes

--- handlers/tasks.go
+++ handlers/tasks.go
@@ -1,7 +1,33 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/Thifany-Nicastro/Go-Docker/database"
+	"github.com/Thifany-Nicastro/Go-Docker/models"
+)
 
 func Home(c *fiber.Ctx) error {
     return c.SendString("Hi")
+}
+
+func CreateTask(c *fiber.Ctx) error {
+    task := new(models.Task)
+
+    if err := c.BodyParser(task); err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "message": err.Error(),
+        })
+    }
+
+    database.DB.Db.Create(&task)
+
+    return c.Status(200).JSON(task)
+}
+
+func ListTasks(c *fiber.Ctx) error {
+    tasks := []models.Task{}
+	
+    database.DB.Db.Find(&tasks)
+
+    return c.Status(200).JSON(tasks)
 }
